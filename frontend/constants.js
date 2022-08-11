b078bea582142508ecaf21cf719fc23f3b5fd281
Mide Sofek <midesofek@gmail.com>
2022-08-12 00:44:38 +0100

    celo marketplace dApp

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/midesofek/celonftmarketplace";
+
+export const MARKETPLACE_ADDRESS = "0xd66cF0cab17107ddD4783FF48E7C4B57C805DE6f";
